6ca959c1dad26b47a432cb563d5acf60c26342c1
Nicolas Siqueira <nicolas2906ss@gmail.com>
2023-01-20 15:17:39 -0300

    enter button is now accepted

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const Ls = localStorage
 let list
 
 function setList(){
+    //this fucntion read the list, set it as an array with needed, else its load the local storage inside list
+    //alert: this function should be called only one time
     if(Ls.getItem("items") == null){
         Ls.setItem("items", "[]")
         setList()
@@ -16,21 +18,25 @@ function setList(){
 }
 
 function setStorage() {
+    //this function set the items from array list in the local storage
     Ls.setItem("items", JSON.stringify(list))
 }
 
 function addItem(item){
+    //This function add an item to the list and to local storage, then its print the new list
     list.push(item)
     setStorage()
     printList()
 }
 function delItem(item){
+    //This function remove an item from list and from the local storage, then its print the new list
     list.splice(item, 1)
     setStorage()
     printList()
 }
 
 function printList(){
+    //This function clear the printed list and then print the new list
     listEl.innerHTML = null
     for (let i = 0; i < list.length; i++){
         listEl.innerHTML = listEl.innerHTML + "<li class='item'><p class='item-text'>" + list[i] + "</p>" + "<button class='delete-button' onClick='delItem("+ i +")'><img class='delete-image' src='./media/icons/close.svg' alt='x'></button></li>"
@@ -41,11 +47,22 @@ setList()
 printList()
 
 
+//when the addButtonEl got clicked, its read the value of the text input when call the addItem() function
 addButtonEl.addEventListener("click", ()=>{
     if (inputEl.value == ""){
         return null
     }else{
         addItem(inputEl.value); inputEl.value = ""
+    }
+})
+//when Enter Key got clicked, its read the value of the text input when call the addItem() function
+inputEl.addEventListener('keypress', (e)=>{
+    if (e.code == 'Enter') {
+        if (inputEl.value == "") {
+            return null
+        }else{
+            addItem(inputEl.value); inputEl.value = ""
+        }
     }
 })
 
