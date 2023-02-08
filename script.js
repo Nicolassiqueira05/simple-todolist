d7973b021c62550d6b7b48728ed32e81d6f15f31
Nicolas Siqueira <nicolas2906ss@gmail.com>
2023-02-08 16:54:14 -0300

    optimized functions to follow solid principles

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const Ls = localStorage
 let list
 
 function setList(){
-    //this function read the list, set it as an array with needed, else its load the local storage inside list
+    //this function read the list, set it as an array when need, else its load the local storage inside list
     //alert: this function should be called only one time
     if(Ls.getItem("items") == null){
         Ls.setItem("items", "[]")
@@ -23,23 +23,19 @@ function setStorage() {
 }
 
 function addItem(item){
-    //This function add an item to the list and to local storage, then its print the new list
+    //This function add an item to the list
     list.push(item)
-    setStorage()
-    printList()
 }
 function delItem(item){
-    //This function remove an item from list and from the local storage, then its print the new list
+    //This function remove an item from list
     list.splice(item, 1)
-    setStorage()
-    printList()
 }
 
 function printList(){
-    //This function clear the printed list and then print the new list
+    //This function refresh the list
     listEl.innerHTML = null
     for (let i = 0; i < list.length; i++){
-        listEl.innerHTML = listEl.innerHTML + "<li class='item'><p class='item-text'>" + list[i] + "</p>" + "<button class='delete-button' onClick='delItem("+ i +")'><img class='delete-image' src='./media/icons/close.svg' alt='x'></button></li>"
+        listEl.innerHTML = listEl.innerHTML + "<li class='item'><p class='item-text'>" + list[i] + "</p>" + "<button class='delete-button' onClick='delItem("+ i +"); setStorage(); printList()'><img class='delete-image' src='./media/icons/close.svg' alt='x'></button></li>"
     }
 }
 
@@ -47,21 +43,25 @@ setList()
 printList()
 
 
-//when the addButtonEl got clicked, its read the value of the text input when call the addItem() function
+//when the addButtonEl got clicked, its read the value of the text input then call addItem(), setStorage(), printList()
 addButtonEl.addEventListener("click", ()=>{
     if (inputEl.value == ""){
         return null
     }else{
         addItem(inputEl.value); inputEl.value = ""
+        setStorage()
+        printList()
     }
 })
-//when Enter Key got clicked, its read the value of the text input when call the addItem() function
+//when Enter Key got clicked, its read the value of the text input when call the addItem(), setStorage(), printList()
 inputEl.addEventListener('keypress', (e)=>{
     if (e.code == 'Enter') {
         if (inputEl.value == "") {
             return null
         }else{
             addItem(inputEl.value); inputEl.value = ""
+            setStorage()
+            printList()
         }
     }
 })
